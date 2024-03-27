96d12aa179fa039e2206bbc6ed633e3e6c411cda
M Waqas <email@example.com>
2024-03-27 09:46:39 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FaFacebookF } from "react-icons/fa";
+export default function page() {
+  return (
+    <div className='bg-blue-700'>
+           
+         
+    </div>
+  )
+}
